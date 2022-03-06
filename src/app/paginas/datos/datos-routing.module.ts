0306877ff5a716337006893ab3b220d7b168924a
lestradavaz <20393136@utcancun.edu.mx>
2022-03-06 14:03:04 -0500

    initial commit

--- src/app/paginas/datos/datos-routing.module.ts
+++ src/app/paginas/datos/datos-routing.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DatosPage } from './datos.page';
+import { VerUsuarioComponent } from '../../componentes/ver-usuario/ver-usuario.component';
+import { EditarUsuarioComponent } from '../../componentes/editar-usuario/editar-usuario.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DatosPage
+    component: DatosPage,
+    children: [
+      {
+        path: '',
+        component: VerUsuarioComponent
+      },
+      {
+        path: 'editar',
+        component: EditarUsuarioComponent
+      }
+    ]
   }
 ];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
